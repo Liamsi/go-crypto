9f5eabfb11417de93e5e619287eba7399f095312
Liamsi <Liamsi@users.noreply.github.com>
2018-06-07 22:25:29 -0700

    reactivate and adapt some tests

--- keys/keybase_test.go
+++ keys/keybase_test.go
@@ -1,156 +1,163 @@
 package keys_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/tendermint/go-crypto"
 	"github.com/tendermint/go-crypto/keys"
+
+	dbm "github.com/tendermint/tmlibs/db"
 )
 
 // TestKeyManagement makes sure we can manipulate these keys well
 func TestKeyManagement(t *testing.T) {
+	// TODO(ismail): understand and update or delete these tests here!
 
-	//// make the storage with reasonable defaults
-	//cstore := keys.New(
-	//	dbm.NewMemDB(),
-	//)
-	//
-	//algo := keys.AlgoEd25519
-	//n1, n2, n3 := "personal", "business", "other"
-	//p1, p2 := "1234", "really-secure!@#$"
-	//
-	//// Check empty state
-	//l, err := cstore.List()
-	//require.Nil(t, err)
-	//assert.Empty(t, l)
-	//
-	//// create some keys
-	//i, err := cstore.Get(n1)
-	//fmt.Println(i)
-	//assert.Error(t, err)
-	//i, _, err = cstore.CreateMnemonic(n1, "english", p1, algo)
-	//require.Equal(t, n1, i.Name)
-	//require.Nil(t, err)
-	//_, _, err = cstore.CreateMnemonic(n2, "english", p2, algo)
-	//require.Nil(t, err)
-	//
-	//// we can get these keys
-	//i2, err := cstore.Get(n2)
-	//assert.Nil(t, err)
-	//_, err = cstore.Get(n3)
-	//assert.NotNil(t, err)
-	//
-	//// list shows them in order
-	//keyS, err := cstore.List()
-	//require.Nil(t, err)
-	//require.Equal(t, 2, len(keyS))
-	//// note these are in alphabetical order
-	//assert.Equal(t, n2, keyS[0].Name)
-	//assert.Equal(t, n1, keyS[1].Name)
-	//assert.Equal(t, i2.PubKey, keyS[0].PubKey)
-	//
-	//// deleting a key removes it
-	//err = cstore.Delete("bad name", "foo")
-	//require.NotNil(t, err)
-	//err = cstore.Delete(n1, p1)
-	//require.Nil(t, err)
-	//keyS, err = cstore.List()
-	//require.Nil(t, err)
-	//assert.Equal(t, 1, len(keyS))
-	//_, err = cstore.Get(n1)
-	//assert.NotNil(t, err)
+	// make the storage with reasonable defaults
+	cstore := keys.New(
+		dbm.NewMemDB(),
+	)
 
-	// make sure that it only signs with the right password
-	// tx := mock.NewSig([]byte("mytransactiondata"))
-	// err = cstore.Sign(n2, p1, tx)
-	// assert.NotNil(t, err)
-	// err = cstore.Sign(n2, p2, tx)
-	// assert.Nil(t, err, "%+v", err)
-	// sigs, err := tx.Signers()
-	// assert.Nil(t, err, "%+v", err)
-	// if assert.Equal(t, 1, len(sigs)) {
-	// 	assert.Equal(t, i2.PubKey, sigs[0])
-	// }
+	algo := keys.AlgoSecp256k1
+	n1, n2, n3 := "personal", "business", "other"
+	p1, p2 := "1234", "really-secure!@#$"
+
+	// Check empty state
+	l, err := cstore.List()
+	require.Nil(t, err)
+	assert.Empty(t, l)
+
+	_, _, err = cstore.CreateMnemonic(n1, "english", p1, keys.AlgoEd25519)
+	assert.Errorf(t, err, "ed25519 keys are currently not supported by keybase")
+
+	// create some keys
+	i, err := cstore.Get(n1)
+	fmt.Println(i)
+	assert.Error(t, err)
+	i, _, err = cstore.CreateMnemonic(n1, "english", p1, algo)
+
+	require.NoError(t, err)
+	require.Equal(t, n1, i.Name)
+	_, _, err = cstore.CreateMnemonic(n2, "english", p2, algo)
+	require.NoError(t, err)
+
+	// we can get these keys
+	i2, err := cstore.Get(n2)
+	assert.Nil(t, err)
+	_, err = cstore.Get(n3)
+	assert.NotNil(t, err)
+
+	// list shows them in order
+	keyS, err := cstore.List()
+	require.NoError(t, err)
+	require.Equal(t, 2, len(keyS))
+	// note these are in alphabetical order
+	assert.Equal(t, n2, keyS[0].Name)
+	assert.Equal(t, n1, keyS[1].Name)
+	assert.Equal(t, i2.PubKey, keyS[0].PubKey)
+
+	// deleting a key removes it
+	err = cstore.Delete("bad name", "foo")
+	require.NotNil(t, err)
+	err = cstore.Delete(n1, p1)
+	require.NoError(t, err)
+	keyS, err = cstore.List()
+	require.NoError(t, err)
+	assert.Equal(t, 1, len(keyS))
+	_, err = cstore.Get(n1)
+	assert.Error(t, err)
+
+	//make sure that it only signs with the right password
+	//tx := mock.NewSig([]byte("mytransactiondata"))
+	//err = cstore.Sign(n2, p1, tx)
+	//assert.NotNil(t, err)
+	//err = cstore.Sign(n2, p2, tx)
+	//assert.Nil(t, err, "%+v", err)
+	//sigs, err := tx.Signers()
+	//assert.Nil(t, err, "%+v", err)
+	//if assert.Equal(t, 1, len(sigs)) {
+	//	assert.Equal(t, i2.PubKey, sigs[0])
+	//}
 }
 
 // TestSignVerify does some detailed checks on how we sign and validate
 // signatures
 func TestSignVerify(t *testing.T) {
-	//cdc, _ := bip39.MustLoadCodec("english")
-	//// make the storage with reasonable defaults
-	//cstore := keys.New(
-	//	dbm.NewMemDB(),
-	//	bip39.MustLoadCodec("english"),
-	//)
-	//algo := keys.AlgoSecp256k1
-	//
-	//n1, n2, n3 := "some dude", "a dudette", "dude-ish"
-	//p1, p2, p3 := "1234", "foobar", "foobar"
-	//
-	//// create two users and get their info
-	//i1, _, err := cstore.CreateMnemonic(n1, p1, algo)
-	//require.Nil(t, err)
-	//
-	//i2, _, err := cstore.CreateMnemonic(n2, p2, algo)
-	//require.Nil(t, err)
-	//
-	//// Import a public key
-	//armor, err := cstore.ExportPubKey(n2)
-	//require.Nil(t, err)
-	//cstore.ImportPubKey(n3, armor)
-	//i3, err := cstore.Get(n3)
-	//require.Nil(t, err)
-	//require.Equal(t, i3.PrivKeyArmor, "")
-	//
-	//// let's try to sign some messages
-	//d1 := []byte("my first message")
-	//d2 := []byte("some other important info!")
-	//d3 := []byte("feels like I forgot something...")
-	//
-	//// try signing both data with both keys...
-	//s11, pub1, err := cstore.Sign(n1, p1, d1)
-	//require.Nil(t, err)
-	//require.Equal(t, i1.PubKey, pub1)
-	//
-	//s12, pub1, err := cstore.Sign(n1, p1, d2)
-	//require.Nil(t, err)
-	//require.Equal(t, i1.PubKey, pub1)
-	//
-	//s21, pub2, err := cstore.Sign(n2, p2, d1)
-	//require.Nil(t, err)
-	//require.Equal(t, i2.PubKey, pub2)
-	//
-	//s22, pub2, err := cstore.Sign(n2, p2, d2)
-	//require.Nil(t, err)
-	//require.Equal(t, i2.PubKey, pub2)
-	//
-	//// let's try to validate and make sure it only works when everything is proper
-	//cases := []struct {
-	//	key   crypto.PubKey
-	//	data  []byte
-	//	sig   crypto.Signature
-	//	valid bool
-	//}{
-	//	// proper matches
-	//	{i1.PubKey, d1, s11, true},
-	//	// change data, pubkey, or signature leads to fail
-	//	{i1.PubKey, d2, s11, false},
-	//	{i2.PubKey, d1, s11, false},
-	//	{i1.PubKey, d1, s21, false},
-	//	// make sure other successes
-	//	{i1.PubKey, d2, s12, true},
-	//	{i2.PubKey, d1, s21, true},
-	//	{i2.PubKey, d2, s22, true},
-	//}
-	//
-	//for i, tc := range cases {
-	//	valid := tc.key.VerifyBytes(tc.data, tc.sig)
-	//	assert.Equal(t, tc.valid, valid, "%d", i)
-	//}
-	//
-	//// Now try to sign data with a secret-less key
-	//_, _, err = cstore.Sign(n3, p3, d3)
-	//assert.NotNil(t, err)
+	cstore := keys.New(
+		dbm.NewMemDB(),
+	)
+	algo := keys.AlgoSecp256k1
+
+	n1, n2, n3 := "some dude", "a dudette", "dude-ish"
+	p1, p2, p3 := "1234", "foobar", "foobar"
+
+	// create two users and get their info
+	i1, _, err := cstore.CreateMnemonic(n1, "english", p1, algo)
+	require.Nil(t, err)
+
+	i2, _, err := cstore.CreateMnemonic(n2, "english", p2, algo)
+	require.Nil(t, err)
+
+	// Import a public key
+	armor, err := cstore.ExportPubKey(n2)
+	require.Nil(t, err)
+	cstore.ImportPubKey(n3, armor)
+	i3, err := cstore.Get(n3)
+	require.Nil(t, err)
+	require.Equal(t, i3.PrivKeyArmor, "")
+
+	// let's try to sign some messages
+	d1 := []byte("my first message")
+	d2 := []byte("some other important info!")
+	d3 := []byte("feels like I forgot something...")
+
+	// try signing both data with both keys...
+	s11, pub1, err := cstore.Sign(n1, p1, d1)
+	require.Nil(t, err)
+	require.Equal(t, i1.PubKey, pub1)
+
+	s12, pub1, err := cstore.Sign(n1, p1, d2)
+	require.Nil(t, err)
+	require.Equal(t, i1.PubKey, pub1)
+
+	s21, pub2, err := cstore.Sign(n2, p2, d1)
+	require.Nil(t, err)
+	require.Equal(t, i2.PubKey, pub2)
+
+	s22, pub2, err := cstore.Sign(n2, p2, d2)
+	require.Nil(t, err)
+	require.Equal(t, i2.PubKey, pub2)
+
+	// let's try to validate and make sure it only works when everything is proper
+	cases := []struct {
+		key   crypto.PubKey
+		data  []byte
+		sig   crypto.Signature
+		valid bool
+	}{
+		// proper matches
+		{i1.PubKey, d1, s11, true},
+		// change data, pubkey, or signature leads to fail
+		{i1.PubKey, d2, s11, false},
+		{i2.PubKey, d1, s11, false},
+		{i1.PubKey, d1, s21, false},
+		// make sure other successes
+		{i1.PubKey, d2, s12, true},
+		{i2.PubKey, d1, s21, true},
+		{i2.PubKey, d2, s22, true},
+	}
+
+	for i, tc := range cases {
+		valid := tc.key.VerifyBytes(tc.data, tc.sig)
+		assert.Equal(t, tc.valid, valid, "%d", i)
+	}
+
+	// Now try to sign data with a secret-less key
+	_, _, err = cstore.Sign(n3, p3, d3)
+	assert.NotNil(t, err)
 }
 
 /*
